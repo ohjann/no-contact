5dc7c284012e13880d530cff1dd5f37bd16b520c
Eoghan Hynes <eoghan@hyn.es>
2024-09-23 21:44:00 +0100

    Dockerise app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # No contact
 A one-way, only two people ever, encrypted chat app
+
+Run using `docker-compose up`
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "@remix-run/react";
+import useSound from "use-sound";
+
+import clickSfx from "../assets/sounds/click1.mp3";
+
 function App() {
+  const [playClick] = useSound(clickSfx, { volume: 0.5 });
+
   return (
     <div>
-      <h1 className="text-white">Hello</h1>
+      <h1 className="text-white">
+        <Link to="id-check" onClick={() => playClick()}>
+          Hello
+        </Link>
+      </h1>
     </div>
   );
 }
